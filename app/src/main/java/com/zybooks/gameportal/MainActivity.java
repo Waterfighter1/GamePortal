ccdb45b93d8ade28b84b73808eb55ad6af69dd13
Waterfighter1 <chloe.gertner.cg@gmail.com>
2023-05-03 09:17:58 -0500

    Name Select Screen WIP

--- app/src/main/java/com/zybooks/gameportal/MainActivity.java
+++ app/src/main/java/com/zybooks/gameportal/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -15,6 +16,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btn = (Button) findViewById(R.id.next);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Moving On", Toast.LENGTH_SHORT).show();
+
+                Intent nextScreen = new Intent(MainActivity.this, gameselect.class);
+                startActivity(nextScreen);
+            }
+        });
 
     }
 }
